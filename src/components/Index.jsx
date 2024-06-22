0c5d6da538ead50924493917dce1a81eb7962c06
DidoReis <djdidoreis@gmail.com>
2024-06-22 14:48:23 -0300

    update-1

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
 import Footer from "./Footer"
+import styles from "../styles";
 
 const Index = () => {
   return (
-    <div className="h-[auto]">
+    <div className={`${styles.boxWidth}`}>
         <Navbar/>
         <Carousel/>
         <Footer/>
